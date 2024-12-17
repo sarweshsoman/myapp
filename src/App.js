fa43542da0ef7e86f4348e793d47143b5c2c6b0b
Sarwesh Satchidanand Soman <89741903+sarweshsoman@users.noreply.github.com>
2024-12-17 16:24:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import AnimRoutes from './components/AnimRoutes';
 import { BrowserRouter as Router } from 'react-router-dom';
 //import { motion } from 'framer-motion';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <Router>
      
         <Header />
+        <Home />
         <AnimRoutes />
       
     </Router>
